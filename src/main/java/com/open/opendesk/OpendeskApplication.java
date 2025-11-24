74abf6bb9d0df53e946e3f9d99168fa3e5080f47
cesa <joaovictorcsatu@gmail.com>
2025-11-24 10:32:12 -0300

    Initial commit

---
+++ src/main/java/com/open/opendesk/OpendeskApplication.java
@@ -0,0 +1,13 @@
+package com.open.opendesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpendeskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpendeskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/open/opendesk/OpendeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.open.opendesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpendeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
